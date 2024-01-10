1bb0d44b87ac22d8a24707a594e1422dd4745a80
Sophie Gamer <sophiergamer@gmail.com>
2024-01-10 11:03:45 -0500

    updating info page

--- src/Routes/Info.js
+++ src/Routes/Info.js
@@ -1,9 +1,18 @@
 export default function Info(){
 return(
     <div id="middle">
-        <h1 id="headText">Here is important information:</h1>
+      
         <h2>Our Ceremony</h2>
+
         <h2>Our Celebration</h2>
+        <h3>August 24, 2024</h3>
+        <p>
+            Please join us at our home 
+            <a href='https://www.google.com/maps/place/566+45th+St,+Brooklyn,+NY+11220/@40.6468146,-74.0085158,17z/data=!3m1!4b1!4m6!3m5!1s0x89c25ac9d3d5ecd1:0x295d75d029c5973!8m2!3d40.6468106!4d-74.0059409!16s%2Fg%2F11c1fnmr10?entry=ttu'>
+                566 45th Street, Brooklyn</a>
+            for an all-day, open-house celebration of our marriage. 
+            Activities will be scheduled throughout the day.
+        </p>
     </div>
 )
     
